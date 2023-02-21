7c3dda8fa10f577b0bab79502b8561c7c29d85b8
alexmtx <1430807+AlexMTX@users.noreply.github.com>
2023-02-21 20:08:37 +0400

    full content in readme

--- make_full_content.py
+++ make_full_content.py
@@ -12,6 +12,7 @@ for filename in sorted(os.listdir(f"./chapters")):
                 line = line.replace("#", f"chapters/{filename}#")
                 toc.append(line)
 
-with open("full_content.md", "w") as f:
-    f.write("# Full content\n")
+with open("README.md", "w") as f:
+    f.write("# Python Questions for Senior and Lead roles\n")
+    f.write("""<img src="https://user-images.githubusercontent.com/67960818/159924991-ad7ac6de-facf-4cb0-a8c9-31a7407fb9e4.png" alt="python-logo-master-v3-TM-flattened" style="max-width: 100%;">\n\n""")
     f.writelines(toc)
